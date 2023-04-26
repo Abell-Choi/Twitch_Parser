97bf44d2b10c3b1e1d67745c5e6fc9ac0b567973
Abellelus-7074 <trooperalex92@gmail.com>
2023-04-27 08:22:50 +0900

    add override ToString()

--- Twitch_Parser/Structor.cs
+++ Twitch_Parser/Structor.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Twitch_Parser
 {
-
-
     public class CHANNEL_INFO_JAR
     {
         public string           B_LOGIN_PK; public string? B_LANG;
@@ -11,6 +12,21 @@ namespace Twitch_Parser
         public DateTime? ADD_AT;
 
         public CHANNEL_INFO_JAR() { this.ADD_AT = DateTime.Now; }
+
+        public string get_convert_tag_list ( ) {
+            return JsonConvert.SerializeObject(this.B_TAGS);
+        }
+
+        public override string ToString() {
+            return JsonConvert.SerializeObject(
+                new{
+                    B_LOGIN_PK = this.B_LOGIN_PK,   B_LANG = this.B_LANG,
+                    D_NAME = this.D_NAME,           B_ID = this.B_ID,
+                    B_TAGS = this.B_TAGS,           THUMB_URL = this.THUMB_URL,
+                    ADD_AT = this.ADD_AT
+                }
+            );
+        }
     }
 
     public class STREAM_STATUS_JAR
@@ -20,6 +36,17 @@ namespace Twitch_Parser
         public string       TITLE;      public int VIEW_COUNT;
         public DateTime     START_AT;   public string THUMB_IMG;
         public DateTime?    UPDATE_AT;  // 디비로 부터 받아온 시간
+
+        public override string ToString(){
+            return JsonConvert.SerializeObject(
+                new{
+                    B_LOGIN_PK = this.B_LOGIN_PK,   B_ID = this.B_ID,
+                    G_ID = this.G_ID,               G_NAME = this.G_NAME,
+                    TITLE = this.TITLE,             VIEW_COUNT = this.VIEW_COUNT,
+                    START_AT = this.START_AT,       THUMB_IMG = this.THUMB_IMG,
+                    UPDATE_AT = UPDATE_AT
+                }
+            );
+        }
     }
 }
-
